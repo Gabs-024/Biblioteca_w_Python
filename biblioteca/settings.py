136639bcc44ad2ddac52fe8b890e37e39c758ea1
Gabs <dev.gabsferreira@gmail.com>
2024-08-11 22:46:33 -0300

    Atualizado ate video 05

--- biblioteca/settings.py
+++ biblioteca/settings.py
@@ -73,6 +73,9 @@ TEMPLATES = [
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
             ],
+        "LIBRARIES":{
+            "filters": "livro.templatetags.filters",
+            }
         },
     },
 ]
--- usuario/forms.py
+++ usuario/forms.py
@@ -26,21 +26,22 @@ class UserForm(forms.ModelForm):
 
     class Meta:
         model = User
-        fields = ('first_name',
-                    'last_name',
-                    'username',
-                    'password',
-                    'password2',
-                    'email')
+        fields = ('nome',
+                    'matricula',
+                    'data_nascimento',
+                    'email',
+                    'telefone',
+                    'senha',
+                    'confirma_senha')
     
     def clean (self, *args, **kwargs):
         cleaned = super().clean()
         validation_error_msgs = {}
 
-        usuario_data = cleaned.get('username')
+        usuario_data = cleaned.get('nomee')
         email_data = cleaned.get('email')
-        password_data = cleaned.get('password')
-        password2_data = cleaned.get('password2')
+        password_data = cleaned.get('senha')
+        password2_data = cleaned.get('confirma_senha')
 
         usuario_db = User.objects.filter(username=usuario_data).first()
         email_db = User.objects.filter(email=email_data).first()
@@ -54,7 +55,7 @@ class UserForm(forms.ModelForm):
         if self.usuario:
             if usuario_db:
                 if usuario_data != usuario_db.username:
-                    validation_error_msgs['username'] = error_msg_user_exists
+                    validation_error_msgs['nome'] = error_msg_user_exists
 
             if email_db:
                 if email_data != email_db.email:
@@ -62,29 +63,29 @@ class UserForm(forms.ModelForm):
 
             if password_data:
                 if password_data != password2_data:
-                    validation_error_msgs['password'] = error_msg_password_match
-                    validation_error_msgs['password2'] = error_msg_password_match
+                    validation_error_msgs['senha'] = error_msg_password_match
+                    validation_error_msgs['confirma_senha'] = error_msg_password_match
                 if len(password_data) < 8:
-                    validation_error_msgs['password'] = error_msg_password_short
+                    validation_error_msgs['senha'] = error_msg_password_short
         else:
             if usuario_db:
-                validation_error_msgs['username'] = error_msg_user_exists
+                validation_error_msgs['nome'] = error_msg_user_exists
 
             if email_db:
                 validation_error_msgs['email'] = error_msg_email_exists
 
             if not password_data:
-                validation_error_msgs['password'] = error_msg_required_field
+                validation_error_msgs['senha'] = error_msg_required_field
 
             if not password2_data:
-                validation_error_msgs['password2'] = error_msg_required_field
+                validation_error_msgs['confirma_senha'] = error_msg_required_field
 
             if password_data != password2_data:
-                validation_error_msgs['password'] = error_msg_password_match
-                validation_error_msgs['password2'] = error_msg_password_match
+                validation_error_msgs['senha'] = error_msg_password_match
+                validation_error_msgs['confirma_senha'] = error_msg_password_match
 
             if len(password_data) < 8:
-                validation_error_msgs['password'] = error_msg_password_short
+                validation_error_msgs['senha'] = error_msg_password_short
 
         if validation_error_msgs:
             raise(forms.ValidationError(validation_error_msgs))
