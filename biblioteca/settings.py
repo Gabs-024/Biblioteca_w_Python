24bc3c598634b003cd2d27c5cc0a0b919c6a0a56
Gabs <dev.gabsferreira@gmail.com>
2024-08-30 11:08:43 -0300

    Layout com imagens Rodando Liso!

--- biblioteca/settings.py
+++ biblioteca/settings.py
@@ -138,7 +138,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'templates/static')
 ]
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'templates/static/media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 MESSAGE_TAGS = {
--- livro/models.py
+++ livro/models.py
@@ -1,3 +1,4 @@
+from typing import Iterable
 from django.db import models
 from django.utils import timezone
 
@@ -15,9 +16,17 @@ class Livro(models.Model):
     titulo = models.CharField(max_length=100, default="Sem título")
     edicao = models.CharField(max_length=15, default=1)
     autor = models.CharField(max_length=100, null=True, blank=True)
+    descricao = models.TextField(max_length=500, null=True, blank=True, 
+                default=("Aqui vai uma breve descrição sobre a historia do livro," 
+                        "falando sobre quem é o personagem principal, qual a sua"
+                        "historia, o que vamos encontrar e entao deixar no ar o" 
+                        "que so podemos saber se ler-mos. este texto ajuda o"
+                        "usuario a decidir se tem interesse ou nao em ler esse" 
+                        "livro ou se ele contem as informacoes que precisa."))
     categoria = models.ForeignKey(Categoria, on_delete=models.DO_NOTHING, null=True, blank=True)
     data_cadastro = models.DateField(default=timezone.now)
     emprestado = models.BooleanField(default=False)
 
     def __str__(self):
         return self.titulo
+                        
